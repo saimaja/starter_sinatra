0a52f6051cbedd3a6f95ab882e1f2360db9e21de
saimaja <saimaakhtar@gmail.com>
2020-05-14 15:55:50 -0400

    finished applicants and started interviewers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 class ApplicationController < Sinatra::Base
 
     set :views, 'app/views'
+    
     get '/' do
         erb :index
     end
 
     get '/about' do
         erb :about
-    end
-
-    get '/applicants' do
-        @applicants = Applicant.all
-        erb :applicants
     end
 
     get '/applicants/new' do
@@ -27,18 +23,44 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/applicants/:id' do
-        binding.pry
         @applicant = Applicant.find(params[:id])
         erb :applicant_profile
     end
 
+    get '/applicants/:id/edit' do
+        @applicant = Applicant.find(params[:id])
+        erb :edit
+      end
 
+    patch '/applicants/:id' do
+        name = params[:name]
+        resume = params[:resume]
+        @applicant = Applicant.find(params[:id])
+        @applicant.update(name: name, resume: resume)
+        redirect "/applicants/#{params[:id]}"
+    end
 
+    delete '/applicants/:id' do 
+        Applicant.delete(params[:id])
+        redirect "/applicants"
+    end
+    
+    get '/applicants' do
+        @applicants = Applicant.all
+        erb :applicants
+    end
 
-   
-   
-    get '/interviewer/new' do 
+    get '/interviewers' do
+        @interviewers = Interviewer.all
+        erb :interviewers
+    end
+    get '/interviewers/new' do 
         erb :new_interviewer
+    end
+
+    get '/interviewers/:id' do
+        @interviewer = Interviewer.find(params[:id])
+        erb :interviewer_profile
     end
 
 end
